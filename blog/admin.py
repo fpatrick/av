614d0c7db4ca6ae32cb1b36d3ea334211ea42dff
erick <fpatrick.erick@gmail.com>
2022-07-27 22:06:44 +0100

    Add post list info to django admin listing

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-#Eu add abaixo
+# Eu add abaixo
 from .models import Post
 from django_summernote.admin import SummernoteModelAdmin
 
@@ -7,6 +7,13 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
+    # Repopulate slug field with title field
+    prepopulated_fields = {'slug': ('title',)}
+    # Add a box on the side to filer by
+    list_filter = ('status', 'created_on')
+    # List posts showing those infos on list
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
     summernote_fields = ('content')
 
 
