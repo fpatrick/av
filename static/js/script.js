f1436b433ef964fb7d848c09d9fe08a8f58cca8f
erick <fpatrick.erick@gmail.com>
2022-08-25 19:07:13 +0100

    Add class to code elements

--- static/js/script.js
+++ static/js/script.js
@@ -3,3 +3,7 @@ setTimeout(function () {
     let alert = new bootstrap.Alert(messages);
     alert.close();
 }, 4000)
+
+let allCodes = document.getElementsByTagName("pre");
+
+allCodes.classList.add("mystyle");
