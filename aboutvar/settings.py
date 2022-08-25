106bff697d0c473b0e58e2f9cb8e2769f7b0cb71
erick <fpatrick.erick@gmail.com>
2022-08-25 20:17:47 +0100

    disable debug

--- aboutvar/settings.py
+++ aboutvar/settings.py
@@ -33,7 +33,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # To summernote work with cross-origin resource sharing (CORS)
 x_FRAME_OPTIONS = 'SAMEORIGIN'
